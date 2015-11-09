b1318e3023f9ed9f8ff48ff24754653caa945421
nataliyamiller <nataliyamiller82@gmail.com>
2015-11-08 18:40:18 -0800

    Adds List Adapter class

---
+++ app/src/main/java/com/epicodus/golfcourse/Hole.java
@@ -0,0 +1,30 @@
+package com.epicodus.golfcourse;
+
+/**
+ * Created by nataliyamiller on 11/8/15.
+ */
+public class Hole {
+    private String mLabel;
+    private int mStrokeCount;
+
+    public Hole(String label, int strokeCount) {
+        mLabel = label;
+        mStrokeCount = strokeCount;
+    }
+
+    public String getLabel() {
+        return mLabel;
+    }
+
+    public void setLabel(String label) {
+        mLabel = label;
+    }
+
+    public int getStrokeCount() {
+        return mStrokeCount;
+    }
+
+    public void setStrokeCount(int strokeCount) {
+        mStrokeCount = strokeCount;
+    }
+}
--- app/src/main/java/com/epicodus/golfcourse/MainActivity.java
+++ app/src/main/java/com/epicodus/golfcourse/MainActivity.java
@@ -1,16 +1,31 @@
 package com.epicodus.golfcourse;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    private Hole[] mHoles = new Hole[18];
+    private ListAdapter mListAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize holes
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++) {
+            mHoles[i] = new Hole("Hole " + (i + 1) + " :", strokes);
+
+        }
+
+        mListAdapter = new ListAdapter(this, mHoles);
+        setListAdapter(mListAdapter);
     }
 
 
