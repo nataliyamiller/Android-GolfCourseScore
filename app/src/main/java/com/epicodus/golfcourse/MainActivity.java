b8d91911de8a93f85ace346f93df6a1334940e2f
nataliyamiller <nataliyamiller82@gmail.com>
2015-11-08 22:42:03 -0800

    Adds menu to clear score

--- app/src/main/java/com/epicodus/golfcourse/MainActivity.java
+++ app/src/main/java/com/epicodus/golfcourse/MainActivity.java
@@ -45,4 +45,36 @@ public class MainActivity extends ListActivity {
         }
         mEditor.apply();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_clear_strokes) {
+
+            mEditor.clear();
+            mEditor.apply();
+
+            for (Hole hole : mHoles) {
+                hole.setStrokeCount(0);
+            }
+
+            mListAdapter.notifyDataSetChanged();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
